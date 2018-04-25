f12eaa69e2328b0f3eec73e69916af7f7ccfd3f1
mjin <ming.jin@orbitz.com>
2018-04-25 13:12:20 -0500

    Finished chapter 3.

--- src/main/java/chapter_3/CollectionUtilities.java
+++ src/main/java/chapter_3/CollectionUtilities.java
@@ -1,8 +1,10 @@
 package chapter_3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 
 public class CollectionUtilities {
     public static <T> List<T> list() {
@@ -17,5 +19,75 @@ public class CollectionUtilities {
     @SafeVarargs
     public static <T> List<T> list(T... t) {
         return Collections.unmodifiableList(Arrays.asList(Arrays.copyOf(t, t.length)));
+    }
+
+    // Exercise 3.4
+    static <T> T head(List<T> list) {
+        if (list.isEmpty()) {
+            throw new IllegalStateException("head of empty list");
+        }
+        return list.get(0);
+    }
+
+    static <T> List<T> tail(List<T> list) {
+        if (list.isEmpty()) {
+            throw new IllegalStateException("head of empty list");
+        }
+        List<T> copy = copy(list);
+        copy.remove((0));
+        return Collections.unmodifiableList(copy);
+    }
+
+    static <T> List<T> append(List<T> list, T t) {
+        List<T> ts = copy(list);
+        ts.add(t);
+        return Collections.unmodifiableList(ts);
+    }
+
+    private static <T> List<T> copy(List<T> list) {
+        return new ArrayList<>(list);
+    }
+
+    // Exercise 3.6
+    static <T, R> R foldLeft(List<T> list, R acc, Function<R, Function<T, R>> f) {
+        if (!list.isEmpty()) {
+            acc = foldLeft(tail(list), f.apply(acc).apply(head(list)), f);
+        }
+        return acc;
+    }
+
+    // Exercise 3.8
+    static <T, R> R foldRight(List<T> list, R acc, Function<T, Function<R, R>> f) {
+        return list.isEmpty() ? acc
+                : f.apply(head(list)).apply(foldRight(tail(list), acc, f));
+    }
+
+    // Exercise 3.9
+    static <T> List<T> reverse(List<T> list) {
+        return foldRight(list, Collections.emptyList(), x -> y -> append(y, x));
+    }
+
+    static <T> List<T> reverseViLeftFold(List<T> list) {
+        return foldLeft(list, list(), x -> y -> foldLeft(x, list(y), a -> b -> append(a, b)));
+    }
+
+    // Exercise 3.10
+    static <T, U> List<U> mapViaFoldLeft(List<T> list, Function<T, U> f) {
+        return foldLeft(list, list(), x -> y -> append(x, f.apply(y)));
+    }
+
+    static <T, U> List<U> mapViaFoldRight(List<T> list, Function<T, U> f) {
+        return foldRight(reverse(list), list(), x -> y -> append(y, f.apply(x)));
+    }
+
+    static List<Integer> range(int start, int end) {
+        return unfold(Collections.emptyList(), start, a -> a + 1, a -> a < end);
+    }
+
+    private static <T> List<T> unfold(List<T> acc, T seed, Function<T, T> f, Function<T, Boolean> p) {
+        if (p.apply(seed)) {
+            return unfold(append(acc, seed), f.apply(seed), f, p);
+        }
+        return acc;
     }
 }
--- src/main/java/chapter_3/EmailValidation.java
+++ src/main/java/chapter_3/EmailValidation.java
@@ -8,21 +8,34 @@ import static chapter_3.Case.*;
 
 public class EmailValidation {
     private static Pattern emailPattern = Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
-    private static Effect<String> success = s -> System.out.println("Mail sent to " + s);
-    private static Effect<String> failure = s -> System.err.println("Error message logged: " + s);
+    private static Function<String, Result> emailChecker = s ->
+            s == null
+                    ? new Result.Failure("email must not be null")
+                    : s.length() == 0
+                    ? new Result.Failure("email must not be empty")
+                    : emailPattern.matcher(s).matches()
+                    ? new Result.Success<>(null)
+                    : new Result.Failure("email " + s + " is invalid.");
 
     public static void main(String... args) {
-        emailChecker.apply("this.is@my.email").bind(success, failure);
-        emailChecker.apply(null).bind(success, failure);
-        emailChecker.apply("").bind(success, failure);
-        emailChecker.apply("john.doe@acme.com").bind(success, failure);
+        validate("this.is@my.email").exec();
+        validate(null).exec();
+        validate("").exec();
+        validate("john.doe@acme.com").exec();
     }
 
-    private static Function<String, Result<String>> emailChecker = s -> match(
-            mcase(() -> Result.success(s)),
-            mcase(() -> s == null, () -> Result.failure("email must not be null")),
-            mcase(() -> s.length() == 0, () -> Result.failure("email must not be empty")),
-            mcase(() -> !emailPattern.matcher(s).matches(), () ->
-                    Result.failure("email " + s + " is invalid."))
-                                                                             );
+    private static void logError(String s) {
+        System.err.println("Error message logged: " + s);
+    }
+
+    private static void sendVerificationMail(String s) {
+        System.out.println("Mail sent to " + s);
+    }
+
+    private static Executable validate(String s) {
+        Result result = emailChecker.apply(s);
+        return (result instanceof Result.Success)
+                ? () -> sendVerificationMail(s)
+                : () -> logError(((Result.Failure)result).getMessage());
+    }
 }
