aba4560888a2f4ce186b397912f34e0ca4638890
Ming Jin <zejin@expedia.com>
2018-05-05 20:21:41 -0500

    Refactor: Replaced duplicated patterns with functions

--- src/main/java/experiment/Dates.java
+++ src/main/java/experiment/Dates.java
@@ -3,34 +3,34 @@ package experiment;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 class Dates {
     private static final int DATE = 1;
     private static final int PUBLISH_HOUR = 12;
     private static final int PUBLISH_MINUTE = 0;
-    static final DateTimeFormatter DDF = DateTimeFormatter.ofPattern("M/d/yy");
     private static final DateTimeFormatter SDF = DateTimeFormatter.ofPattern("MMddyy");
+    static final DateTimeFormatter DDF = DateTimeFormatter.ofPattern("M/d/yy");
     static final DateTimeFormatter LDF = DateTimeFormatter.ofPattern("MMMM d, yyyy");
 
     static LocalDateTime getPublishDate(String[] elements) {
-        if (isEvent(elements)) {
-            return null;
-        }
-        return previousDate(LocalDate.parse(elements[DATE], SDF));
+        return getDate(elements, () -> null, Dates::previousDate);
     }
 
     static String getChallengeDate(String[] elements) {
-        if (isEvent(elements)) {
-            return LocalDate.now().format(LDF);
-        }
-        return LDF.format(LocalDate.parse(elements[DATE], SDF));
+        return getDate(elements, () -> LocalDate.now().format(LDF), LDF::format);
     }
 
     static String getChallengeDateForDesc(String[] elements) {
+        return getDate(elements, () -> LocalDate.now().format(DDF), DDF::format);
+    }
+
+    private static <A> A getDate(String[] elements, Supplier<A> eventDate, Function<LocalDate, A> otherDate) {
         if (isEvent(elements)) {
-            return LocalDate.now().format(DDF);
+            return eventDate.get();
         }
-        return DDF.format(LocalDate.parse(elements[DATE], SDF));
+        return otherDate.apply(LocalDate.parse(elements[DATE], SDF));
     }
 
     private static boolean isEvent(String[] elements) {
